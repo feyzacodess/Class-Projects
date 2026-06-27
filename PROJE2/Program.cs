8a35af2f82500624fafb8bd396f29b75897c94fd
Feyza Özen <feyzaozen1903@gmail.com>
2026-06-27 22:11:54 +0300

    Add files via upload

---
+++ PROJE2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PROJE2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // İKİ SAYININ ARİTMETİK ORTALAMASINI BULAN PROGRAM
+
+            double sayi1;
+            double sayi2;
+            string Metin1;
+            string Metin2;
+            double Sonuc;
+
+            Console.WriteLine("1. sayıyı giriniz: ");
+            Metin1 = Console.ReadLine();
+            sayi1 = Convert.ToDouble(Metin1);
+
+            Console.WriteLine("2. sayıyı giriniz: ");
+            Metin2 = Console.ReadLine();
+            sayi2 = Convert.ToDouble(Metin2);
+
+            Sonuc = sayi1 + sayi2 / 2d;
+
+            Console.WriteLine("Girilen sayıların aritmetik ortalaması {0} olur", sayi1, sayi2, Sonuc);
+
+        }
+    }
+}
---
+++ PROJE3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PROJE3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 İLE 10 ARASINDAKİ SAYILARIN TOPLAMINI BULAN PROGRAM
+            try
+            {
+                int Sonuc = 0;
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Sonuc += i;
+                }
+                Console.WriteLine("1 ile 10 arasındaki sayıların toplamı {0} olur.",Sonuc);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
